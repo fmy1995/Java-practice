5859e70e45628bd627ae02c95e4230fba39f72d0
fmy1995 <74345039+fmy1995@users.noreply.github.com>
2021-04-26 17:15:03 +0900

    Add files via upload

---
+++ wcp/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "JAVA!!";
+        System.out.println(str);
+        str = "NOT SCRIPT";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
