6d279f0c836166d7215962e45200b5fb01494ca0
fmy1995 <74345039+fmy1995@users.noreply.github.com>
2021-05-04 21:13:03 +0900

    chapter10

---
+++ wcp/Exception01.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class Exception01 {
+    public static void main(String[] args) {
+        System.out.println("100 / 0 = ");
+        int result = 100 / 0;
+        System.out.println("result" + result);
+        System.out.println("program close");
+    }
+}
